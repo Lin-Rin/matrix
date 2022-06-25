a16fa30f383ea6a38e7f28c6fb8c3b884e29f5b8
linrin <qyvjc1@gmail.com>
2022-06-25 08:34:32 +0300

    done + test

---
+++ src/main/Multiplication.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Multiplicator {
+    public Matrix multiply(Matrix matrix1, Matrix matrix2) {
+        int vertical = matrix1.getVerticalSize();
+        int horizontal = matrix2.getHorizontalSize();
+        int controlSize = matrix1.getHorizontalSize();
+
+        Matrix result = new Matrix(vertical, horizontal);
+
+        for (int i = 0; i < vertical; i++) {
+            for (int j = 0; j < horizontal; j++) {
+                int value = 0;
+                for (int k = 0; k < controlSize; k++) {
+                    value += matrix1.getElement(i, k) * matrix2.getElement(k, j);
+                }
+                result.setElement(i, j, value);
+            }
+        }
+
+        return result;
+    }
+}
